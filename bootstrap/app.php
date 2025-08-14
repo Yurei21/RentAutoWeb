2c822802e041bddc94010f4a2c57d40999ecd136
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-14 17:16:28 +0800

    fixed it

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,12 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')
+                ->prefix('admin')
+                ->group(base_path('routes/adminAuth.php'));
+        }
     )
-    ->withMiddleware(function ($middleware) {
-        $middleware->alias([
-            'auth.admin' => \App\Http\Middleware\AdminAuthenticate::class,
-        ]);
-    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
