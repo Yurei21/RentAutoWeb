9f75945db05955091da8398c13494e3f8328ae73
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-16 17:49:08 +0800

    resources incoming (backend)

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class DocumentsResource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,10 +14,6 @@ class DocumentsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'user_id' => new UserResource($this->user),
-            
-        ];
+        return parent::toArray($request);
     }
 }
