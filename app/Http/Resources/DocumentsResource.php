8ca3e5184f72e86f4bf873c1d46d24eb723ee662
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-18 19:26:15 +0800

    document resource done

--- app/Http/Resources/DocumentsResource.php
+++ app/Http/Resources/DocumentsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
@@ -19,7 +20,8 @@ class DocumentsResource extends JsonResource
             'id' => $this->id,
             'user_id' => new UserResource($this->user),
             'document_type' => $this->document_type,
-            'document_path' => $this->document_path ? Storage::url($this->document_path) : '';
+            'document_path' => $this->document_path ? Storage::url($this->document_path) : '',
+            'upload_date' => (new Carbon($this->upload_date))->format('Y-m-d'),
         ];
     }
 }
