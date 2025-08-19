d6f927a2e879222712edbf648ab532bb62e304ed
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-19 19:06:59 +0800

    finished maintenance resource

--- app/Http/Resources/MaintenaceResource.php
+++ app/Http/Resources/MaintenaceResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,12 @@ class MaintenaceResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'vehicle_id' => new VehicleResource($this->vehicle_id),
+            'maintenance_date' => (new Carbon($this->maintenance_date))->format('Y-m-d'),
+            'details' => $this->details,
+            'cost' => $this->cost,
+        ];
     }
 }
