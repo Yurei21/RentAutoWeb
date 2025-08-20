51ae4397718485bba914b359ec0f0d65c40f3172
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-20 21:45:30 +0800

    payments resource (one step at a time

--- app/Http/Resources/PaymentsResource.php
+++ app/Http/Resources/PaymentsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,14 @@ class PaymentsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'rental_id' => new RentalsResource($this->rental_id),
+            'amount_paid' => $this->amount_paid,
+            'payment_method' => $this->payment_method,
+            'payment_date' => (new Carbon($this->payment_date))->format('Y-m-d'),
+            'pay_status' => $this->pay_status,
+            'additionalOrLate_fee' => $this->additionalOrLate_fee,
+        ];
     }
 }
