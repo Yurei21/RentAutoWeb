71c27d629b97f52ccb1f37b43445cc71d6ed9749
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-21 18:35:36 +0800

    another step at a time

--- app/Http/Resources/RentalsResource.php
+++ app/Http/Resources/RentalsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,16 @@ class RentalsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => new UserResource($this->user_id),
+            'vehicle_id' => new VehicleResource($this->vehicle_id),
+            'rental_start_date' => (new Carbon($this->rental_start_date))->format('Y-m-d'),
+            'rental_end_date' => (new Carbon($this->rental_end_date))->format('Y-m-d'),
+            'total_cost' => $this->total_cost,
+            'payment_status' => $this->payment_status,
+            'status' => $this->status,
+            'barcode' => $this->barcode,
+        ];
     }
 }
