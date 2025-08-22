69f45989bb38a145b8d0726695e84f0461faf849
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-22 08:29:23 +0800

    vehicle resources done

--- app/Http/Resources/VehicleResource.php
+++ app/Http/Resources/VehicleResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class VehicleResource extends JsonResource
 {
@@ -14,6 +15,13 @@ class VehicleResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'model' => $this->model,
+            'rent_price' => $this->rent_price,
+            'availabilty_status' => $this->availability_status,
+            'license_plate' => $this->license_plate,
+            'car_path' => $this->car_path ? Storage::url($this->car_path) : '',
+        ];
     }
 }
