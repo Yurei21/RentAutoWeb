abd99b9c0a997702570a92d62f5e88935e3b2dd1
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-26 22:15:16 +0800

    separating controllers from admin and user (on-going)

---
+++ app/Http/Controllers/AdminDocumentsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Documents;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAdminDocumentsRequest;
+use App\Http\Requests\UpdateAdminDocumentsRequest;
+
+class AdminDocumentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdminDocumentsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Documents $documents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Documents $documents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAdminDocumentsRequest $request, Documents $documents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Documents $documents)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/AdminPaymentsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payments;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAdminPaymentsRequest;
+use App\Http\Requests\UpdateAdminPaymentsRequest;
+
+class AdminPaymentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdminPaymentsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Payments $payments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Payments $payments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAdminPaymentsRequest $request, Payments $payments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Payments $payments)
+    {
+        //
+    }
+}
