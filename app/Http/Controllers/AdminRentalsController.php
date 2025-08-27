bb4e10253e8f470c92cb58fdc89f57d404794649
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-08-27 17:58:39 +0800

    Attaching requests to the controllers

---
+++ app/Http/Controllers/AdminRentalsController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rentals;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAdminRentalsRequest;
+use App\Http\Requests\UpdateAdminRentalsRequest;
+
+class AdminRentalsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdminRentalsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rentals $rentals)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Rentals $rentals)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Rentals $rentals)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rentals $rentals)
+    {
+        //
+    }
+}
