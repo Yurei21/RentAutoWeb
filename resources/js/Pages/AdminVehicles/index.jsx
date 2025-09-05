e92f55f4533cae54e17b20a988daa13f89f96ecb
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-09-05 19:26:19 +0800

    ongoing pls

--- resources/js/Pages/AdminVehicles/index.jsx
+++ resources/js/Pages/AdminVehicles/index.jsx
@@ -2,5 +2,12 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedAdminLayout";
 import {Head, Link} from "@inertiajs/react";
 
 export default function Index({}){
-    
+    <AuthenticatedLayout
+        header={
+            <div className="flex justify-between items-center">
+                <h2 className="text-3xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Vehicles</h2>
+            </div>
+        }
+    >
+    </AuthenticatedLayout>
 }
